6ba3f1e6565455e6a17547aef0c962b792bf1953
Jordan Campbell <jordan.evan.campbell@gmail.com>
2018-01-30 18:18:42 +1300

    Tue 30 Jan 2018 18:18:42 NZDT

--- dev.js
+++ dev.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require("fs");
+const http = require("http");
 const argv = require('minimist')(process.argv.slice(2));
 
 if (argv.debug) {
@@ -18,25 +19,55 @@ let parse = function(snapshot) {
     console.log(snapshot)
   }
 
+  for(var idx = 0; idx < dom.length; idx++) {
+    const key = generateKey()
+    dom[ idx ]['key'] = key;
+
+    var children = dom[ idx ]["childNodeIndexes"]
+
+    if (children) {
+      for(var kdx = 0; kdx < children.length; kdx++) {
+        dom[ children[kdx] ]["parentKey"] = key;
+      }
+    }
+
+  }
+
   for(var idx = 0; idx < layout.length; idx++) {
 
-    var nodeName  = dom[ layout[ idx ]['domNodeIndex'] ]['nodeName'];
-    var nodeValue = dom[ layout[ idx ]['domNodeIndex']  ]['nodeValue'];
+    var currentNode = layout[ idx ]['domNodeIndex'];
+
+    var nodeName  = dom[ currentNode ]['nodeName'];
+    var nodeValue = dom[ currentNode  ]['nodeValue'];
     var nodeLayout = layout[ idx ]['boundingBox'];
     var nodeStyle = styles[ layout[ idx ]['styleIndex'] ]
+
+    var key = dom[ currentNode ]['key'];
+    var pkey = dom[ currentNode ]['parentKey'];
+
+    var attr = dom[ currentNode ]['attributes']
+
+    if (!attr) {
+      attr = ''
+    }
+
+    if (!pkey) {
+      pkey = ''
+    }
 
     if (nodeStyle) {
       nodeStyle = nodeStyle['properties']
     }
 
-
     var node = {
-      "nodeName" : nodeName,
-      "nodeValue" : nodeValue,
-      "nodeLayout" : nodeLayout,
-      "nodeStyle" : nodeStyle
+      'nodeName' : nodeName,
+      'nodeValue' : nodeValue,
+      'nodeLayout' : nodeLayout,
+      'nodeStyle' : nodeStyle,
+      'key' : key,
+      'pkey' : pkey,
+      'attr': attr
     };
-
     nodeData.push(node);
 
   }
@@ -60,35 +91,110 @@ var createStyleWhiteList = function() {
   return computedStyles;
 }
 
+function interrogate(node) {
+
+  console.log(node);
+  console.log("=======================================");
+
+  if (node.children) {
+    for (var idx = 0; idx < node.childNodeCount; idx++) {
+      interrogate(node.children[idx])
+    }
+  }
+}
+
+function generateKey() {
+  var text = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  var keyLength = 8;
+
+  for (var i = 0; i < keyLength; i++)
+    text += chars.charAt(Math.floor(Math.random() * chars.length));
+
+  return text;
+}
+
 async function run() {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({headless:false})
   // const browser = await puppeteer.launch()
   const page = await browser.newPage()
   // await page.goto('localhost:3000/')
-  await page.goto(argv['url'])
+  await page.goto(argv.url)
+  await page.setViewport({
+  	width: 1920,
+  	height: 1080
+  });
+
+  var viewport = await page.viewport()
+  console.log(viewport);
   // await page.goto('http://google.co.nz/')
   // await page.goto('https://murmuring-ocean-82758.herokuapp.com/')
+
+  // let bodyHTML = await page.evaluate(() => document.body.outerHTML);
+  const html = await page.evaluate('new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML');
+
+  // console.log( html);
 
   await page._client.send('DOM.enable')
   await page._client.send('CSS.enable')
   await page._client.send('Animation.setPlaybackRate', { playbackRate: 12 });
 
-  // const doc = await page._client.send('DOM.getDocument')
-  // const node = await page._client.send('DOM.querySelector', {nodeId: doc.root.nodeId, selector: 'h1'})
-  // const fonts = await page._client.send('CSS.getPlatformFontsForNode', {nodeId: node.nodeId})
+  const doc = await page._client.send('DOM.getDocument')
+
+  // interrogate(doc.root)
+  const node = await page._client.send('DOM.querySelector', {nodeId: doc.root.nodeId, selector: 'h1'})
+  const fonts = await page._client.send('CSS.getPlatformFontsForNode', {nodeId: node.nodeId})
+
+  // const allNodes = await page._client.send('DOM.querySelectorAll', {
+  //       nodeId: doc.root.nodeId,
+  //       selector: '*'
+  //   });
+  //
+  // console.log(allNodes);
+  //
+  // const stylesForNodes = []
+  // for (id of allNodes.nodeIds) {
+  //   stylesForNodes.push(await page._client.send('CSS.getMatchedStylesForNode', {nodeId: id}));
+  // }
+
+  const hrefs = await page.evaluate(() => {
+    const anchors = document.querySelectorAll('a');
+    return [].map.call(anchors, a => a.href);
+  });
+
+  // console.log(hrefs);
 
   let computedStyles = createStyleWhiteList();
-
   const snapshot = await page._client.send('DOMSnapshot.getSnapshot', {computedStyleWhitelist:computedStyles});
-
+  // console.log(snapshot);
   parse(snapshot)
 
-  await browser.close()
+  // await browser.close()
 }
 
 
 run();
 
-//
-// (async () => {
-// })()
+
+// console.log(http);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,145 @@
 const express = require('express');
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
 var router = express.Router();
 
-// let scrape = async () => {
-//   var data = {
-//     "name": "Jordan",
-//     "url": "atlasreality.xyz",
-//   };
-//
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('http://books.toscrape.com/');
-//   await page.waitFor(1000);
-//   // Scrape
-//
-//   browser.close();
-//   return data
-// };
+const googleFontAPIKey = "AIzaSyDP0GJ5DHi5yKGiJPL8NgEFrAY_LyVeHF8";
+
+let parse = function(snapshot) {
+
+  const dom = snapshot['domNodes'];
+  const layout = snapshot['layoutTreeNodes'];
+  const styles = snapshot['computedStyles'];
+
+  var nodeData = [];
+
+  // generate a unique key for each node
+  // - so that we can refer to a node's parent
+  for(var idx = 0; idx < dom.length; idx++) {
+    const key = generateKey()
+    dom[ idx ]['key'] = key;
+
+    var children = dom[ idx ]["childNodeIndexes"]
+
+    if (children) {
+      for(var kdx = 0; kdx < children.length; kdx++) {
+        dom[ children[kdx] ]["parentKey"] = key;
+      }
+    }
+
+  }
+
+  for(var idx = 0; idx < layout.length; idx++) {
+
+    var currentNode = layout[ idx ]['domNodeIndex'];
+
+    var nodeName  = dom[ currentNode ]['nodeName'];
+    var nodeValue = dom[ currentNode  ]['nodeValue'];
+    var nodeLayout = layout[ idx ]['boundingBox'];
+    var nodeStyle = styles[ layout[ idx ]['styleIndex'] ]
+
+    var key = dom[ currentNode ]['key'];
+    var pkey = dom[ currentNode ]['parentKey'];
+
+    var attr = dom[ currentNode ]['attributes']
+
+    if (!attr) {
+      attr = ''
+    }
+
+    if (!pkey) {
+      pkey = ''
+    }
+
+    if (nodeStyle) {
+      nodeStyle = nodeStyle['properties']
+    }
+
+    var node = {
+      'nodeName' : nodeName,
+      'nodeValue' : nodeValue,
+      'nodeLayout' : nodeLayout,
+      'nodeStyle' : nodeStyle,
+      'key' : key,
+      'pkey' : pkey,
+      'attr': attr
+    };
+    nodeData.push(node);
+
+  }
+
+  return nodeData
+}
+
+function generateKey() {
+  var text = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  var keyLength = 8;
+
+  for (var i = 0; i < keyLength; i++)
+    text += chars.charAt(Math.floor(Math.random() * chars.length));
+
+  return text;
+}
+
+
+var createStyleWhiteList = function() {
+
+  let computedStyles = ['color', 'background-color', 'border-color', 'border-width', 'font-size',
+                        'border-left-color', 'border-left-style', 'border-left-width',
+                        'border-right-color', 'border-right-style', 'border-right-width',
+                        'border-top-color', 'border-top-style', 'border-top-width',
+                        'border-bottom-color', 'border-bottom-style', 'border-bottom-width',
+                        'padding-left', 'padding-right', 'padding-top', 'padding-bottom',
+                        'background-image',
+                        'font-weight'
+                      ];
+  return computedStyles;
+}
+
+async function run(url) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.setViewport({
+    width: 1000,
+    height: 500
+  });
+
+  // await page.setViewport({
+    // width: 1920,
+    // height: 1080
+  // });
+
+  await page.goto(url)
+
+  await page._client.send('DOM.enable')
+  await page._client.send('CSS.enable')
+  await page._client.send('Animation.setPlaybackRate', { playbackRate: 12 });
+
+  const doc = await page._client.send('DOM.getDocument')
+
+  let computedStyles = createStyleWhiteList();
+  const snapshot = await page._client.send('DOMSnapshot.getSnapshot', {computedStyleWhitelist:computedStyles});
+  let output = parse(snapshot)
+
+  await browser.close()
+
+  return output
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Atlas Reality' });
 });
 
-// router.get('/scraper', function(req, res, next) {
-//   scrape().then((data) => {
-//     res.render('scraper', { data: data });
-//   });
-// });
+router.get('/client', function(req, res, next) {
+
+  const url = req.query.atlasurl
+
+  run(url).then((data) => {
+    res.status(200).send(data)
+  });
+});
 
 module.exports = router;
